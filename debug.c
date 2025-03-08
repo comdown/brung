e02836ce17654f2f3a4b18fd0ffb87ad3a20b41f
Bruce <prog3487@gmail.com>
2025-03-08 20:21:04 +0900

    BINARY OP

--- debug.c
+++ debug.c
@@ -38,7 +38,7 @@ int disassembleInstruction(Chunk* chunk, int offset)
 	// print line number
 	// 이전 명령의 줄번호와 같으면 줄번호 다시 출력하지 않음
 	int line = getLine(chunk, offset);
-	if (offset > 0 && getLine(chunk, offset-1))
+	if (offset > 0 && line == getLine(chunk, offset-1))
 	{
 		printf("   | ");
 	}
@@ -53,8 +53,18 @@ int disassembleInstruction(Chunk* chunk, int offset)
 	{
 	case OP_RETURN:
 		return simpleInstruction("OP_RETURN", offset);
+	case OP_NEGATE:
+		return simpleInstruction("OP_NEGATE", offset);
 	case OP_CONSTANT:
 		return constantInstruction("OP_CONSTANT", chunk, offset);
+	case OP_ADD:
+		return simpleInstruction("OP_ADD", offset);
+	case OP_SUBTRACT:
+		return simpleInstruction("OP_SUBTRACT", offset);
+	case OP_MULTIPLY:
+		return simpleInstruction("OP_MULTIPLY", offset);
+	case OP_DIVIDE:
+		return simpleInstruction("OP_DIVIDE", offset);
 	default:
 		printf("Unknown opcode %d\n", instruction);
 		return offset + 1;
--- main.c
+++ main.c
@@ -1,20 +1,43 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, char* argv[])
 {
+	initVM();
+
 	Chunk chunk;
 	initChunk(&chunk);
 
-	int constantIdx = addConstant(&chunk, 1.4);
-	writeChunk(&chunk, OP_CONSTANT, 100);
-	writeChunk(&chunk, constantIdx, 100);
-	writeChunk(&chunk, OP_RETURN, 101);
-	writeChunk(&chunk, OP_RETURN, 101);
+	int constantIdx = addConstant(&chunk, 1.2);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constantIdx, 123);
 
-	disassembleChunk(&chunk, "test chunk");
+	constantIdx = addConstant(&chunk, 3.4);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constantIdx, 123);
 
+	writeChunk(&chunk, OP_ADD, 123);
+
+	constantIdx = addConstant(&chunk, 5.6);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constantIdx, 123);
+
+	writeChunk(&chunk, OP_DIVIDE, 123);
+	writeChunk(&chunk, OP_RETURN, 124);
+
+	//int constantIdx = addConstant(&chunk, 1.4);
+	//writeChunk(&chunk, OP_CONSTANT, 100);
+	//writeChunk(&chunk, constantIdx, 100);
+	//
+	//writeChunk(&chunk, OP_NEGATE, 101);
+	//
+	
+	// writeChunk(&chunk, OP_RETURN, 101);
+
+	interpret(&chunk);
+	freeVM();
 	freeChunk(&chunk);
 
 	return 0;
